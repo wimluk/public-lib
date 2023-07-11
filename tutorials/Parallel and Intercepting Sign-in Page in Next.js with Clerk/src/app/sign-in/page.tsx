849dbbfa4c44bf9e84b18e736d45649e30458b31
wimluk <l.wimhofer@gmail.com>
2023-07-11 09:14:54 +0200

    New Branch

--- tutorials/Parallel and Intercepting Sign-in Page in Next.js with Clerk/src/app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <SignIn afterSignInUrl="/" />
-    </main>
-  );
-}
