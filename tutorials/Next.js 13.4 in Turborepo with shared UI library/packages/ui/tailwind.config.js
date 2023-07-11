e3ce6e28c3e02d48d303d22db0ad805b2112fe3a
wimluk <l.wimhofer@gmail.com>
2023-07-11 08:59:06 +0200

    New Branch

--- tutorials/Next.js 13.4 in Turborepo with shared UI library/packages/ui/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-const sharedConfig = require("tailwindconfig");
-
-module.exports = {
-  ...sharedConfig,
-  content: ["./**/*.{js,ts,jsx,tsx,mdx}"],
-};
